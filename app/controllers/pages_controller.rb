7039e4edffd7531279a031441a08e31ac67e68f1
Rianne Westgeest <riannewestgeest@hotmail.com>
2016-02-29 17:59:00 +0100

    longest word game almost done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,60 @@
+require 'open-uri'
+require 'json'
+
+class PagesController < ApplicationController
+  def game
+    @grid = generate_grid(12).join(" ")
+    @start_time = Time.now.to_i
+  end
+
+  def score
+    @longest_word = params[:longest_word]
+    @grid = params[:grid]
+    @start_time = Time.at(params[:start_time].to_i)
+    @end_time = Time.now
+    @get_translation = get_translation(@longest_word)
+    @included = included?(@longest_word, @grid)
+    @score = run_game(@longest_word, @grid, @start_time, @end_time)
+  end
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a[rand(26)] }
+  end
+
+  def included?(guess, grid)
+    the_grid = grid.clone
+    guess.chars.each do |letter|
+      the_grid.delete_at(the_grid.index(letter)) if the_grid.include?(letter)
+    end
+    grid.size == guess.size + the_grid.size
+  end
+
+  def get_translation(word)
+    response = open("http://api.wordreference.com/0.8/80143/json/enfr/#{word.downcase}")
+    json = JSON.parse(response.read.to_s)
+    json['term0']['PrincipalTranslations']['0']['FirstTranslation']['term'] unless json["Error"]
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+  result = { time: end_time - start_time }
+
+  result[:translation] = get_translation(attempt)
+  result[:score], result[:message] = score_and_message(
+    attempt, result[:translation], grid, result[:time])
+
+  result
+end
+
+  def score_and_message(attempt, translation, grid, time)
+    if translation
+      if included?(attempt.upcase, grid)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not in the grid"]
+      end
+    else
+      [0, "not an english word"]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/game' => 'pages#game'
+  post '/score' => 'pages#score'
+  get '/score' => 'pages#score'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
